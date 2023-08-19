0236f7e441699e05477986b82f8db6753c17cdd3
BHObserver <BHObserver0@gmail.com>
2023-08-19 08:11:00 +0600

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import './style.css';
+
+const todo = [
+    {
+        description: 'Shopping',
+        completed: false,
+        index: 1
+    },
+    {
+        description: 'Breakfast',
+        completed: false,
+        index: 2
+    },
+    {
+        description: 'Exercise',
+        completed: false,
+        index: 3
+    },
+
+]
