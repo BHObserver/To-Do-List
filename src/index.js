f23db8faf250aac78ba6f0008401be2e6f70fd9d
BHObserver <BHObserver0@gmail.com>
2023-08-19 13:17:19 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,26 +3,26 @@ import './style.css';
 const list = document.querySelector('ul');
 
 const todo = [
-    {
-        description: 'Shopping',
-        completed: false,
-        index: 1
-    },
-    {
-        description: 'Breakfast',
-        completed: false,
-        index: 2
-    },
-    {
-        description: 'Exercise',
-        completed: false,
-        index: 3
-    },
-]
+  {
+    description: 'Shopping',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Breakfast',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Exercise',
+    completed: false,
+    index: 3,
+  },
+];
 
-todo.forEach( (item) => {
-    const listItem = document.createElement('li');
-    listItem.innerHTML = `<input class="done" type="checkbox"> <h4 class='todo-item'> ${item.description} </h4>  <i class="fa fa-ellipsis-v" aria-hidden="true"></i> <span> <i class="fa fa-trash"></i> </span>`;
-    /* listItem.innerText = item.description; */
-    list.appendChild(listItem);
+todo.forEach((item) => {
+  const listItem = document.createElement('li');
+  listItem.innerHTML = `<input class="done" type="checkbox"> <h4 class='todo-item'> ${item.description} </h4>  <i class="fa fa-ellipsis-v" aria-hidden="true"></i> <span> <i class="fa fa-trash"></i> </span>`;
+  /* listItem.innerText = item.description; */
+  list.appendChild(listItem);
 });
