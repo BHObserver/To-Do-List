89d0a4b4f13563442d307ac6b7312024a063e9e5
BHObserver <BHObserver0@gmail.com>
2023-08-19 13:17:21 +0600

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,28 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-    mode: 'development',
-    entry: {
-        bundle: path.resolve(__dirname, 'src/index.js'),
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js'
-    },
-    plugins: [
-        new HtmlWebpackPlugin ({
-            title: 'To Do List',
-            filename: 'index.html',
-            template: 'src/template.html'
-        }) 
-    ],
-    module: {
-        rules: [
-          {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-          },
-        ],
+  mode: 'development',
+  entry: {
+    bundle: path.resolve(__dirname, 'src/index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'To Do List',
+      filename: 'index.html',
+      template: 'src/template.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
       },
-}
+    ],
+  },
+};
