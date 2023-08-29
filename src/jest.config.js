6d35c431e476a5864914d05df9090cc01b1446b3
BHObserver <BHObserver0@gmail.com>
2023-08-29 22:45:46 +0600

    Update jest.config.js

--- src/jest.config.js
+++ src/jest.config.js
@@ -1,1 +1,9 @@
-
+export default {
+  transform: {},
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '\\.pnp\\.[^\\/]+$',
+  ],
+  testMatch: ['**/__tests__/**/*.js', '**/?(*.)+(spec|test).js'],
+  moduleNameMapper: {},
+};
