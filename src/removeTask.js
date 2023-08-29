070fce5a7c212d598235e337e132515cc673eec2
BHObserver <BHObserver0@gmail.com>
2023-08-29 22:46:02 +0600

    Update removeTask.js

--- src/removeTask.js
+++ src/removeTask.js
@@ -1,22 +1,3 @@
-/* export default function removeTask(
-  parentElement,
-  tasks,
-  savedHTML,
-  list,
-  updateLocalStorage,
-  updateIndex,
-) {
-  const index = parseInt(parentElement.dataset.index, 10);
-  parentElement.style.opacity = 0;
-  setTimeout(() => {
-    parentElement.remove();
-    tasks.splice(index - 1, 1);
-    savedHTML = list.innerHTML;
-    updateLocalStorage(tasks, savedHTML);
-    updateIndex();
-  }, 500);
-} */
-
 export default function removeTask(parentElement, tasks) {
   const index = parseInt(parentElement.dataset.index, 10);
   const newTasks = tasks.filter((task, i) => i !== index - 1);
