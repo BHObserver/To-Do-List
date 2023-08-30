b15725e74baa98948d2f9f9c7c475159973af4bf
BHObserver <BHObserver0@gmail.com>
2023-08-30 21:34:07 +0600

    Update addTask.js

--- src/addTask.js
+++ src/addTask.js
@@ -1,6 +1,8 @@
 import Task from './task';
 
-export default function addTask(value, tasks) {
+function addTask(value, tasks) {
   const newTasks = [...tasks, new Task(value, false, tasks.length + 1)];
   return newTasks;
 }
+
+module.exports = addTask;
