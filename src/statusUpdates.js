bb343bcd1fdaec313ce5d296b26583a21c55d307
Enoh <eno.col@gmail.com>
2023-08-31 15:39:11 +0100

    Edit statusUpdates

--- src/statusUpdates.js
+++ src/statusUpdates.js
@@ -1,4 +1,4 @@
-const updateStatus = (tasks, index, completed) => {
+export const updateStatus = (tasks, index, completed) => {
   const newTasks = [...tasks]; // Create a copy of tasks array
   if (index >= 0 && index < newTasks.length) {
     newTasks[index].complete = completed;
@@ -6,12 +6,7 @@ const updateStatus = (tasks, index, completed) => {
   return newTasks;
 };
 
-const clearCompletedTasks = (tasks, savedHTML) => {
+export const clearCompletedTasks = (tasks, savedHTML) => {
   const newTasks = tasks.filter((task) => !task.complete);
   return { newTasks, savedHTML };
-};
-
-export default {
-  updateStatus,
-  clearCompletedTasks,
 };
